68e57806dee9477a750269cc23b16be0c05e6705
medzdev <medzdeveloper@gmail.com>
2023-05-07 18:09:19 -0300

    chore: first commit!

---
+++ src/main/java/com/medzdev/animanagerserver/controller/AnimanagerServerApplication.java
@@ -0,0 +1,20 @@
+package com.medzdev.animanagerserver.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AnimanagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimanagerServerApplication.class, args);
+	}
+
+	@GetMapping("/information_records")
+	public String api_root(){
+		return "Hello world!";
+	}
+}
---
+++ src/test/java/com/medzdev/animanagerserver/AnimanagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medzdev.animanagerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimanagerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
