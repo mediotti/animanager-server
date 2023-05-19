d46a208ff97603a564add3d1892d5c5403b65a21
medzdev <medzdeveloper@gmail.com>
2023-05-19 16:57:16 -0300

    chore: adding all the changes to swap OS

--- src/main/java/com/medzdev/animanagerserver/controller/InformationRecordsEndpoints.java
+++ src/main/java/com/medzdev/animanagerserver/controller/InformationRecordsEndpoints.java
@@ -1,12 +1,18 @@
 package com.medzdev.animanagerserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InformationRecordsEndpoints {
     @GetMapping("/information_records")
-    public String getAllRecords(){
-        return "All records";
+    public String getRecords(@RequestParam(required = false) String id){
+        return "ID: " + id;
+    }
+
+    @GetMapping("/information_records/animals")
+    public String getAnimalRecords(@RequestParam(required = false) String id){
+        return "Animal Record ID: " + id;
     }
 }
