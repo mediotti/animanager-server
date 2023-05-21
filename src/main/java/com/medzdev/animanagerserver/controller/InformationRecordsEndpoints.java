153955f9cb4b33cdaff7d55c73b99518a1195b91
Gabriel Mediotti <gabriel.marques@green4t.com>
2023-05-20 23:05:33 -0300

    chore: fixing stuff

--- src/main/java/com/medzdev/animanagerserver/controller/InformationRecordsEndpoints.java
+++ src/main/java/com/medzdev/animanagerserver/controller/InformationRecordsEndpoints.java
@@ -3,16 +3,17 @@ package com.medzdev.animanagerserver.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+enum role {
+    animal,
+    caregiver,
+    employee;
+}
 
 @RestController
 public class InformationRecordsEndpoints {
+    
     @GetMapping("/information_records")
-    public String getRecords(@RequestParam(required = false) String id){
-        return "ID: " + id;
-    }
-
-    @GetMapping("/information_records/animals")
-    public String getAnimalRecords(@RequestParam(required = false) String id){
-        return "Animal Record ID: " + id;
+    public String getInformationRecords(@RequestParam(required = false) role role_param){
+        return "Those are the " + role_param + " information records";
     }
 }
