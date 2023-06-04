bfefafdf6c18226a0736684e032f57fb06eaeb6a
medzdev <medzdeveloper@gmail.com>
2023-06-03 22:58:17 -0300

    chore: Added POST REST method to insert new record files.

--- src/main/java/com/medzdev/animanagerserver/entities/ResponseInformationRecordsDTO.java
+++ src/main/java/com/medzdev/animanagerserver/entities/ResponseInformationRecordsDTO.java
@@ -1,8 +1,18 @@
 package com.medzdev.animanagerserver.entities;
 
-public record ResponseInformationRecordsDTO(Long id, String name, String role) {
+import java.util.UUID;
+
+public record ResponseInformationRecordsDTO(UUID id, String name, String role, String phoneNumber, String ssn,
+                                            Role recordRole, String zipCode, String addressComplements) {
     public ResponseInformationRecordsDTO(InformationRecords informationRecords){
-        this(informationRecords.getId(), informationRecords.getName(), informationRecords.getRecord_role());
+        this(informationRecords.getId(),
+                informationRecords.getFirst_name(),
+                informationRecords.getLast_name(),
+                informationRecords.getPhone_number(),
+                informationRecords.getSsn(),
+                informationRecords.getRecord_role(),
+                informationRecords.getZip_code(),
+                informationRecords.getAddress_complements());
     }
 
 
